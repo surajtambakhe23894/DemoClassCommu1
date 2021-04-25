fd3594480f7e22c62184c431bd816bdca202abbe
user <user@LAPTOP-QOF44QE3>
2021-04-26 01:15:42 +0530

    by suraj1

---
+++ Testing GitHub Demo1/src/com/m1/Test1.java
@@ -0,0 +1,19 @@
+package com.m1;
+
+import com.m2.Test2;
+
+public class Test1 {
+public void m1()
+{
+System.out.println("Test1---m1");	
+}
+public static void main(String[] args) {
+	System.out.println("Test1---main");
+	Test1 t1=new Test1();
+	t1.m1();
+	
+	Test2 t2=new Test2();
+	t2.m2();
+	
+}
+}
---
+++ Testing GitHub Demo1/src/com/m2/Test2.java
@@ -0,0 +1,8 @@
+package com.m2;
+
+public class Test2 {
+public void m2()
+{
+System.out.println("Test2--m2");	
+}
+}
