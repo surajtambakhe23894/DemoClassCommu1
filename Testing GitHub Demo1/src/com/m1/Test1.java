41bee4ef56b7789637f3844fcedad57d5d7b7b4d
user <user@LAPTOP-QOF44QE3>
2021-06-22 01:56:42 +0530

    new sub branch create

--- Testing GitHub Demo1/src/com/m1/Test1.java
+++ Testing GitHub Demo1/src/com/m1/Test1.java
@@ -7,6 +7,10 @@ public void m1()
 {
 System.out.println("Test1---m1");	
 }
+public void m55()
+{
+System.out.println("Test1---m55");	
+}
 public static void main(String[] args) {
 	System.out.println("Test1---main");
 	Test1 t1=new Test1();
